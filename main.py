a04187cd52cd3a8f3c5a740826f4b51a5d1845a9
Shoumik Dey <shoumikdey123@gmail.com>
2020-02-25 15:44:37 +0530

    Storing each sentence in a CSV

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ parsedPDF = parser.from_file("FAA_Emergency_Procedures.pdf")
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 data = parsedPDF['content']
-
+fcsv = open('sent.csv', 'w')
+writer = csv.writer(fcsv, delimiter=',')
 for sent in tokenizer.tokenize(data):
     print(" ".join(sent.split()))
+    fcsv.write(" ".join(sent.split()))
+    fcsv.write("\n")
+    # writer.writerow('')
+    #writer.writerow("Hello")
+
 
 
 nlp = spacy.load('en_core_web_sm')
