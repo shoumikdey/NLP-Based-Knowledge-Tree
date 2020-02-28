a7c8cdabdade40bcc4d2a3f24a8f904fea8e626a
Shoumik Dey <shoumikdey123@gmail.com>
2020-02-28 20:58:13 +0530

    Documentating and adding comments to main.py

--- main.py
+++ main.py
@@ -13,14 +13,27 @@ import os
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
+# Loading the english model from spacy
 nlp = spacy.load('en_core_web_sm')
 
 def main():
+    '''
+    The input pdf is taken from the test_data folder.
+    Only one file will be read from this folder if multiple files are present.
+    The file that appers first in the alphabetical order will be read and others will be ignored.
+    '''
+
     pd.set_option('display.max_colwidth', 200)
     file = os.listdir("test_data"+os.sep)
     file = list(filter(lambda x: x[-4:] == '.pdf', file))
     parsedPDF = parser.from_file("test_data"+os.sep+str(file[0]))
+
+    '''
+    The PDF document is being parsed here and all the text(only) is extracted
+    and tokenized.
+
+    From the text, it is next tokenized into sentences using the natural language toolkit nltk.
+    '''
 
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
@@ -32,7 +45,12 @@ def main():
     sentences = pd.read_csv("sent.csv", sep= "\t", header=None, skip_blank_lines=True)
     sentences[0].shape
 
+
     sentences[0] = cleanup_text(sentences[0])
+    '''
+    Cleaning of the sentences extracted. Punctuations, stop words, pronouns etc are removed
+    so that they do not produce false positives
+    '''
 
     sub_obj = []
     print("Reading", file[0])
@@ -42,10 +60,15 @@ def main():
     print("Extracting relation and creating knowledge graph")
     rel = [get_relation(i) for i in tqdm(sentences[0])]
 
+    '''
+    The entity pairs (subject, object) from each sentence is extracted and the relation for each node
+    of the knowledge graph is obtained.
+    '''
+
     src = [i[0] for i in sub_obj]
     target = [i[1] for i in sub_obj]
 
-    kg_df = pd.DataFrame({'source':src, 'target':target, 'edge':rel})
+    kg_df = pd.DataFrame({'source':src, 'target':target, 'edge':rel}) # The relations obtained are appended into a pandas dataframe for the visualisation of the knowledge graph
     print()
     print("Enter the name of the system")
     print("Example inputs: 'engine', 'fire', 'flap', 'electrical', 'fuel', 'gear'")
@@ -57,8 +80,8 @@ def main():
 
     plt.figure(figsize=(12,12))
     pos = nx.spring_layout(G)
-    nx.draw(G, with_labels=True, node_color='skyblue', edge_cmap=plt.cm.Blues, pos=pos)
-    plt.show()
+    nx.draw(G, with_labels=True, node_color='blue', edge_cmap=plt.cm.Blues, pos=pos)
+    plt.show() # displaying the processed knowledge graph
 
 if __name__ == '__main__':
     main()
