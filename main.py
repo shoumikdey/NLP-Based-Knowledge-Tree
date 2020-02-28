deda6624e31d9950553bd95e32dc9ab28b72ec73
Shoumik Dey <shoumikdey123@gmail.com>
2020-02-28 19:47:24 +0530

    Reading pdf files from test folder

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 import bs4
 import requests
 from spacy import displacy
+import os
 nlp = spacy.load('en_core_web_sm')
 
 from spacy.matcher import Matcher
@@ -17,54 +18,59 @@ import networkx as nx
 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-pd.set_option('display.max_colwidth', 200)
 
-parsedPDF = parser.from_file("FAA_Emergency_Procedures.pdf")
+def main():
+    pd.set_option('display.max_colwidth', 200)
+    file = os.listdir("test_data"+os.sep)
+    file = list(filter(lambda x: x[-4:] == '.pdf', file))
+    parsedPDF = parser.from_file("test_data"+os.sep+str(file[0]))
 
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-data = parsedPDF['content']
-fcsv = open('sent.csv', 'w')
-writer = csv.writer(fcsv, delimiter="\t")
-for sent in tokenizer.tokenize(data):
-    # print(" ".join(sent.split()))
-#     fcsv.write(" ".join(sent.split()))
-#     fcsv.write("\n")
+    data = parsedPDF['content']
+    fcsv = open('sent.csv', 'w')
+    writer = csv.writer(fcsv, delimiter="\t")
+    for sent in tokenizer.tokenize(data):
+        # print(" ".join(sent.split()))
+    #     fcsv.write(" ".join(sent.split()))
+    #     fcsv.write("\n")
 
-    writer.writerow([" ".join(sent.split())])
-#reading from csv
-sentences = pd.read_csv("sent.csv", sep= "\t", header=None, skip_blank_lines=True)
-sentences[0].shape
-
-
-
-#
+        writer.writerow([" ".join(sent.split())])
+    #reading from csv
+    sentences = pd.read_csv("sent.csv", sep= "\t", header=None, skip_blank_lines=True)
+    sentences[0].shape
 
 
-# #
-# doc = nlp("The failure indications of EFIS may be entirely different from conventional instruments making recognition of system malfunction much more difficult for the pilot.")
-# for tok in doc:
-#   print(tok.text, "...", tok.dep_)
-# exit(0)
 
-sentences[0] = cleanup_text(sentences[0])
+    #
 
-sub_obj = []
-for i in tqdm(sentences[0]):
-    sub_obj.append(entity_pair(i))
 
-rel = [get_relation(i) for i in tqdm(sentences[0])]
-#print(pd.Series(rel).value_counts()[:])
+    # #
+    # doc = nlp("The failure indications of EFIS may be entirely different from conventional instruments making recognition of system malfunction much more difficult for the pilot.")
+    # for tok in doc:
+    #   print(tok.text, "...", tok.dep_)
+    # exit(0)
 
-src = [i[0] for i in sub_obj]
-target = [i[1] for i in sub_obj]
+    sentences[0] = cleanup_text(sentences[0])
 
-kg_df = pd.DataFrame({'source':src, 'target':target, 'edge':rel})
-kg_df = kg_df[kg_df['edge']=="engine"]
-print(kg_df)
-G = nx.from_pandas_edgelist(kg_df, "source", "target", edge_attr=True, create_using=nx.MultiDiGraph())
+    sub_obj = []
+    for i in tqdm(sentences[0]):
+        sub_obj.append(entity_pair(i))
 
-plt.figure(figsize=(12,12))
-pos = nx.spring_layout(G)
-nx.draw(G, with_labels=True, node_color='skyblue', edge_cmap=plt.cm.Blues, pos=pos)
-plt.show()
+    rel = [get_relation(i) for i in tqdm(sentences[0])]
+    #print(pd.Series(rel).value_counts()[:])
+
+    src = [i[0] for i in sub_obj]
+    target = [i[1] for i in sub_obj]
+
+    kg_df = pd.DataFrame({'source':src, 'target':target, 'edge':rel})
+    kg_df = kg_df[kg_df['edge']=="engine"]
+    print(kg_df)
+    G = nx.from_pandas_edgelist(kg_df, "source", "target", edge_attr=True, create_using=nx.MultiDiGraph())
+
+    plt.figure(figsize=(12,12))
+    pos = nx.spring_layout(G)
+    nx.draw(G, with_labels=True, node_color='skyblue', edge_cmap=plt.cm.Blues, pos=pos)
+    plt.show()
+
+main()
